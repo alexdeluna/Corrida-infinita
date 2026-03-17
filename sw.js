364729963a5f99c2084d37c2346c0df05feaad28
Alex de Luna <67716472+alexdeluna@users.noreply.github.com>
2026-03-17 11:42:35 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = "desvie-pro-v1"
+
+const files = [
+"/",
+"/index.html",
+"/style.css",
+"/app.js"
+]
+
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open(cacheName)
+.then(cache=>cache.addAll(files))
+)
+})
+
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request)
+.then(res=> res || fetch(e.request))
+)
+})
